48794d759b092f532a88b94f0bec674e57429876
YilunAllenChen <allenchen@gatech.edu>
2020-11-01 20:29:07 -0500

    firmware testing script added

--- firmware.cpp
+++ firmware.cpp
@@ -5,76 +5,80 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "firmware.h"
-#define LED 18 
+#define LED 18
 #define AmotorInA 24
 #define AmotorInB 23
 #define BmotorInA 6
 #define BmotorInB 5
-#define AmotorPWM 12 
+#define AmotorPWM 12
 #define BmotorPWM 13
 
 extern "C"
 {
 	int speed = 0;
-	void initialize_pins(){
-	gpioSetPWMrange(BmotorPWM,100);
-	gpioSetPWMrange(AmotorPWM,100);
-	gpioSetMode(BmotorInA, PI_OUTPUT);
-	gpioSetMode(BmotorInB, PI_OUTPUT);
-	gpioSetMode(BmotorPWM, PI_OUTPUT);
-	gpioSetMode(AmotorInA, PI_OUTPUT);
-	gpioSetMode(AmotorInB, PI_OUTPUT);
-	gpioSetMode(AmotorPWM, PI_OUTPUT);
-}
+	void initialize_pins()
+	{
+		gpioSetPWMrange(BmotorPWM, 100);
+		gpioSetPWMrange(AmotorPWM, 100);
+		gpioSetMode(BmotorInA, PI_OUTPUT);
+		gpioSetMode(BmotorInB, PI_OUTPUT);
+		gpioSetMode(BmotorPWM, PI_OUTPUT);
+		gpioSetMode(AmotorInA, PI_OUTPUT);
+		gpioSetMode(AmotorInB, PI_OUTPUT);
+		gpioSetMode(AmotorPWM, PI_OUTPUT);
+	}
 
-void stop(){
-	gpioWrite(AmotorInA, PI_ON);
-	gpioWrite(AmotorInB, PI_ON);
-	gpioWrite(BmotorInA, PI_ON);
-	gpioWrite(BmotorInB, PI_ON);
-	speed = 0;
-}
-void set_left(int speedIn) {
-	gpioWrite(AmotorInA, speedIn > 0 ? PI_ON : PI_OFF); //yes, pick one after ?, no, pick one after :
-	gpioWrite(AmotorInB, speedIn > 0 ? PI_OFF : PI_ON);
-	speed = speedIn > 0 ? speedIn : -speedIn;
-	gpioPWM(AmotorPWM, speedIn);
-}
+	void stop()
+	{
+		gpioWrite(AmotorInA, PI_ON);
+		gpioWrite(AmotorInB, PI_ON);
+		gpioWrite(BmotorInA, PI_ON);
+		gpioWrite(BmotorInB, PI_ON);
+		speed = 0;
+	}
+	void set_left(int speedIn)
+	{
+		gpioWrite(AmotorInA, speedIn > 0 ? PI_ON : PI_OFF); //yes, pick one after ?, no, pick one after :
+		gpioWrite(AmotorInB, speedIn > 0 ? PI_OFF : PI_ON);
+		speed = speedIn > 0 ? speedIn : -speedIn;
+		gpioPWM(AmotorPWM, speedIn);
+	}
 
-void set_right(int speedIn) {
-	gpioWrite(BmotorInA, speedIn > 0 ? PI_ON : PI_OFF);
-	gpioWrite(BmotorInB, speedIn > 0 ? PI_OFF : PI_ON);
-	speed = speedIn > 0 ? speedIn : -speedIn;
-	gpioPWM(BmotorPWM, speedIn);
-}
+	void set_right(int speedIn)
+	{
+		gpioWrite(BmotorInA, speedIn > 0 ? PI_ON : PI_OFF);
+		gpioWrite(BmotorInB, speedIn > 0 ? PI_OFF : PI_ON);
+		speed = speedIn > 0 ? speedIn : -speedIn;
+		gpioPWM(BmotorPWM, speedIn);
+	}
 
-void set_speed(int speed)
-{ 
-	speed = speed;
-}
+	void set_speed(int speed)
+	{
+		speed = speed;
+	}
 
-void go_straight(){
-	set_left(speed);
-	set_right(speed);
-};
+	void go_straight()
+	{
+		set_left(speed);
+		set_right(speed);
+	};
 
-void go_backward()
-{	
-	set_left(-speed);
-	set_right(-speed);
+	void go_backward()
+	{
+		set_left(-speed);
+		set_right(-speed);
 	}
 	void turn_left()
-{
-	set_left(speed);
-	set_right(-speed);
+	{
+		set_left(speed);
+		set_right(-speed);
 	}
-	
-	void turn_right()
-{
-	set_left(-speed);
-	set_right(+speed);
-}
 
+	void turn_right()
+	{
+		set_left(-speed);
+		set_right(+speed);
+	}
 }
 int main()
 {
---
+++ firmware_testing_script.py
@@ -0,0 +1,107 @@
+'''
+Backend of the DormRover project. Powered by Flask and python/C++ binding utilities
+
+Refernces:
+'''
+
+import ctypes
+import pathlib
+from time import sleep
+
+# Import the shared library
+try:
+    libname = pathlib.Path().absolute() / "firmware.so"
+    firmware = ctypes.CDLL(libname)
+except:
+    print("ERROR: firmware.so not supplied. \nExiting...")
+    exit()
+
+# Function return and parameter types declaration
+try:
+    firmware.set_speed.argtypes = [ctypes.c_int]
+    # firmware.get_velocity_x.restype = ctypes.c_float
+    # firmware.get_velocity_y.restype = ctypes.c_float
+    # firmware.get_acceleration_x.restype = ctypes.c_float
+    # firmware.get_acceleration_y.restype = ctypes.c_float
+    # firmware.get_temperature.restype = ctypes.c_float
+except Exception as e:
+    print("ERROR: Initialization failed for one of the functions: {}\n Exiting...".format(e))
+    exit()
+
+
+# def get_IMU() -> dict:
+#     '''
+#     Function uses pre-built c++ libraries and obtain all data obtained by the IMU sensor.
+
+#     :return: returns a dictionary of all data.
+#         Example format: 
+#         {
+#             'velocity_x': 1.0,
+#             'velocity_y': 1.5,
+#             'humidity': 0.63
+#         }
+#     '''
+#     res = {}
+#     res['velocity_x'] = firmware.get_velocity_x()
+#     res['velocity_y'] = firmware.get_velocity_y()
+#     res['acceleration_x'] = firmware.get_acceleration_x()
+#     res['acceleration_y'] = firmware.get_acceleration_y()
+#     return res
+
+# def get_temperature() -> float:
+#     '''
+#     Function uses pre-built c++ libraries and obtain data obtained by the temperature sensor.
+
+#     :return: data obtained by the temperature sensor
+#     '''
+#     return firmware.get_temperature()
+
+def set_speed(speed: float) -> None:
+    '''
+    Function uses pre-built c++ libraries and set the speed of both motors to parameter speed.
+
+    :param speed: The target speed, from 0 to 100.
+    '''
+    firmware.set_speed(ctypes.c_float(speed))
+
+
+def go_forward() -> None:
+    '''
+    Function uses pre-built c++ libraries and let both motors go forward.
+    '''
+    firmware.go_forward()
+
+def go_backward() -> None:
+    '''
+    Function uses pre-built c++ libraries and let both motors go backward.
+    '''
+    firmware.go_backward()
+
+def turn_left() -> None:
+    '''
+    Function uses pre-built c++ libraries and let the robot turn left.
+    '''
+    firmware.turn_left()
+
+def turn_right() -> None:
+    '''
+    Function uses pre-built c++ libraries and let the robot turn right.
+    '''
+    firmware.turn_right()
+
+def stop() -> None:
+    '''
+    Function uses pre-built c++ libraries and let the robot go to a hard stop.
+    '''
+    firmware.stop()
+
+
+
+
+if __name__ == '__main__':
+    go_forward()
+    sleep(1)
+    stop()
+    turn_left()
+    sleep(1)
+    stop()
