e535aaefd88dde7680bd43761a5e943f9c5b861c
YilunAllenChen <allenchen@gatech.edu>
2020-11-04 13:12:56 -0500

    camera utils updated

--- camera_utils.py
+++ camera_utils.py
@@ -3,14 +3,22 @@
 # http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
 import io
-import picamera
+# import picamera
+# camera = picamera.PiCamera()
+# def vid_gen():
+#     while True:
+#         cam_stream = io.BytesIO()
+#         frame = cam.capture(cam_stream, 'jpeg')
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + cam_stream.getvalue() + b'\r\n')
 
 
-cam = picamera.PiCamera()
+from camera_pi import Camera
 
 def vid_gen():
+    camera = Camera()
+    """Video streaming generator function."""
     while True:
-        cam_stream = io.BytesIO()
-        frame = cam.capture(cam_stream, 'jpeg')
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + cam_stream.getvalue() + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
