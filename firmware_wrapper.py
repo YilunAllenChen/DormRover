30ee2ad44a2aae6a0fff5a49fae80a4a1b4c0274
YilunAllenChen <allenchen@gatech.edu>
2020-11-04 19:47:54 -0500

    downgraded speed

--- firmware_wrapper.py
+++ firmware_wrapper.py
@@ -86,7 +86,7 @@ except Exception as e:
 
 # System initialization
 motion_control_lib.initialize_pins()
-motion_control_lib.set_speed(10)
+motion_control_lib.set_speed(20)
 imu = IMU_lib.LSM9DS1_create()
 IMU_lib.LSM9DS1_begin(imu)
 if IMU_lib.LSM9DS1_begin(imu) == 0:
