478bedaed7780de46356d4bc8eae7ca67dc6d2d3
CharlesChu <zhenjie@vt.edu>
2020-11-08 01:01:11 -0500

    Add base version fronend
    Frontend first revision with basic functionalities.
    
    Still diging in the javascript cave
    
    Planed revision with bootstrap for better organization

---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+
+
+
+
+document.getElementsByName("speed"); // returns a list of elements with name="textbox1"
+// document.getElementsByName("speed")[0] // returns the first element in DOM with name="textbox1"
+
+
