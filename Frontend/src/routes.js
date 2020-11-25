b1b9d52de75cc6dea78e060b2f88d24c2e038bb1
CharlesChu <zhenjie@vt.edu>
2020-11-25 16:23:03 -0500

    revert navi menu
    revert to earlier version to fix navi bar

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import DashboardLayout from 'src/layouts/DashboardLayout';
 import MainLayout from 'src/layouts/MainLayout';
 import DashboardView from 'src/views/DashboardView';
+import NotFoundView from 'src/views/errors/NotFoundView';
 import ProjectDescriptionView from 'src/views/ProjectDescriptionView';
 import TeamView from 'src/views/TeamView';
 
 const routes = [
   {
-    path: '/',
-    element: <MainLayout />,
+    path: 'app',
+    element: <DashboardLayout />,
     children: [
-      { path: '/', element: <Navigate to="/dashboard" /> },
       { path: 'dashboard', element: <DashboardView /> },
       { path: 'projectdescription', element: <ProjectDescriptionView /> },
       { path: 'team', element: <TeamView /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '404', element: <NotFoundView /> },
+      { path: '/', element: <Navigate to="/app/dashboard" /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   }
