fcbdecd15e34cc8549ff087b28b79885c2fd587c
YilunAllenChen <allenchen@gatech.edu>
2020-11-25 20:30:14 -0500

    production build for servo control added

--- Frontend/build/precache-manifest.7d954df5809494197c7987e841b8ad39.js
+++ Frontend/build/precache-manifest.7d954df5809494197c7987e841b8ad39.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95ea3402495ad0cbb4860e053abe8f50",
+    "revision": "6486e26538e9d2066eea515c722c82c5",
     "url": "/index.html"
   },
   {
-    "revision": "d0049adb2a94d507ab99",
+    "revision": "94826d293d47e4c995a9",
     "url": "/static/css/2.18fb2ca0.chunk.css"
   },
   {
-    "revision": "d0049adb2a94d507ab99",
-    "url": "/static/js/2.f6634e71.chunk.js"
+    "revision": "94826d293d47e4c995a9",
+    "url": "/static/js/2.f724d3bb.chunk.js"
   },
   {
     "revision": "44e39e61fb6a0450f7c8014318774752",
-    "url": "/static/js/2.f6634e71.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f724d3bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90d701dcb08e4b4e706b",
-    "url": "/static/js/main.9bce7b1d.chunk.js"
+    "revision": "02faa2de7f62ee595fa0",
+    "url": "/static/js/main.afd9e07d.chunk.js"
   },
   {
     "revision": "02abd13eaf1d7c33000d",
